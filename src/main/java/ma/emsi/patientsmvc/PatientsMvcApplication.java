7e37b2d2205aa6d437476047a5d2be87cd7977bb
John Doe <johndoe@example.com>
2022-04-10 23:06:46 +0000

    FC

---
+++ src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,39 @@
+package ma.emsi.patientsmvc;
+
+import ma.emsi.patientsmvc.entities.Patient;
+import ma.emsi.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+    //@Bean //cad que cette méth va s'executer au démarrage.
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(
+                    new Patient(null,"Hassan",new Date(),false,122));
+            patientRepository.save(
+                    new Patient(null,"Mohammed",new Date(),true,321));
+            patientRepository.save(
+                    new Patient(null,"Yasmine",new Date(),true,165));
+            patientRepository.save(
+                    new Patient(null,"Hanae",new Date(),false,132));
+
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+
+        };
+    }
+
+}
